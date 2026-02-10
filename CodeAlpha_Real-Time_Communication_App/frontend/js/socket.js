72c9062392e757a0926136da381653a3bd743414
ItsMontey <shreyashrane125@gmail.com>
2026-02-10 10:01:43 +0530

    temp

--- CodeAlpha_Real-Time_Communication_App/frontend/js/socket.js
+++
@@ -1,6 +0,0 @@
-const socket = io("http://localhost:5000");
-const roomId = new URLSearchParams(window.location.search).get("room");
-
-socket.on("connect", () => {
-  socket.emit("join-room", roomId);
-});
